7d4986919ceb1aedbbfaa61c28beba608b4167f5
lizhi-a <2507332458@qq.com>
2021-02-13 19:46:08 +0800

    下载模块

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ var app = express()
 app.use('/public/',express.static(path.join(__dirname,'./public/')))
 app.use('/node_modules/',express.static(path.join(__dirname,'./node_modules/')))
 
+// 在 Node 中有很多模板引擎可以使用，不是只有 art-template
+//  ejs,jade(pug),handlebars,nunjucks
+app.engine('html',require('express-art-template'))
+app.set('views',path.join(__dirname,'./views/'))  //默认是 ./views
+
 app.get('/',function(req,res){
-    res.send('hello')
+    res.render('index.html',{
+        name:'lizhi'
+    })
 })
 
 app.listen(5000,function(){
